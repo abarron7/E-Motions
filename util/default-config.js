69d3147a102801c79fb61c5cf8a7a74a9f2e99d4
Alex Barron <abarron4544@yahoo.com>
2019-03-21 20:34:34 -0600

    probable error for authentication fix

--- util/default-config.js
+++ util/default-config.js
@@ -1,7 +1,7 @@
 export default {
   oidc: {
     clientId: 'oadjrwyhSqaqWDTz356',
-    issuer: 'https://Student-dev-817020.com/oauth2/default',
+    issuer: 'https://dev-817020.okta.com/oauth2/default',
     redirectUri: 'http://localhost:8080/implicit/callback',
     scope: 'openid profile email',
   },
