22ef500bf107950968d5186c4bc79119b7ac93c3
Alex Barron <abarron4544@yahoo.com>
2019-03-21 19:27:20 -0600

    Basic layout and authentication

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Security, ImplicitCallback } from '@okta/okta-react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './components/pages/Home'
+import Data from './components/pages/Data'
+
+const config = {
+  issuer: 'https://Student-dev-817020.okta.com/oauth2/default',
+  redirect_uri: window.location.origin + '/implicit/callback',
+  client_id: '0oadjrwyhSqaqWDTz356'
+};
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <Security issuer={config.issuer}
+                  client_id={config.client_id}
+                  redirect_uri={config.redirect_uri}
+        >
+          <Route path='/' exact={true} component={Home}/>
+          <Route path='/data' exact={true} component={Data}/>
+          <Route path='/implicit/callback' component={ImplicitCallback}/>
+        </Security>
+      </Router>
     );
   }
 }
