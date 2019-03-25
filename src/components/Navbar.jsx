4e5f232d93594bd17acb1f4e6431b7a37a87f803
Alex Barron <abarron4544@yahoo.com>
2019-03-25 11:17:46 -0600

    updated authentication

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { withAuth } from '@okta/okta-react';
 import React, { Component } from 'react';
-import { Container, Image, Menu } from 'semantic-ui-react';
+import { Container, Image, Icon, Menu } from 'semantic-ui-react';
 import { checkAuthentication } from '../helpers';
 
 export default withAuth(class Navbar extends Component {
@@ -41,6 +41,7 @@ export default withAuth(class Navbar extends Component {
             {this.state.authenticated === true && <Menu.Item id="profile-button" as="a" href="/profile">Profile</Menu.Item>}
             {this.state.authenticated === true && <Menu.Item id="logout-button" as="a" onClick={this.logout}>Logout</Menu.Item>}
             {this.state.authenticated === false && <Menu.Item as="a" onClick={this.login}>Login</Menu.Item>}
+            {this.state.authenticated === true && <Menu.Item id="messages-button" as="a" href="/messages"><Icon name="mail outline" />Messages</Menu.Item>}
           </Container>
         </Menu>
       </div>
