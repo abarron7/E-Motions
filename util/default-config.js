5a7f88ee6ec2d9e2eba7b0e74b2ca501e244aa66
Alex Barron <abarron4544@yahoo.com>
2019-03-28 20:48:10 -0600

    possible redirect fix

--- util/default-config.js
+++ util/default-config.js
@@ -2,7 +2,7 @@ export default {
   oidc: {
     clientId: 'oadjrwyhSqaqWDTz356',
     issuer: 'https://dev-817020.okta.com/oauth2/default',
-    redirectUri: 'https://feels-1point0.herokuapp.com/implicit/callback',
+    redirectUri: window.location.origin + '/implicit/callback',
     scope: 'openid profile email',
   }
 };
