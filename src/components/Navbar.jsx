fbd5f867bb9dd6d45f802fc295c4f92859d6d5a9
Alex Barron <abarron4544@yahoo.com>
2019-03-28 20:08:52 -0600

    Finally working Okta

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,18 @@
+/*
+ * Copyright (c) 2018, Okta, Inc. and/or its affiliates. All rights reserved.
+ * The Okta software accompanied by this notice is provided pursuant to the Apache License, Version 2.0 (the "License.")
+ *
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+
 import { withAuth } from '@okta/okta-react';
 import React, { Component } from 'react';
-import { Container, Image, Icon, Menu } from 'semantic-ui-react';
+import { Container, Icon, Image, Menu } from 'semantic-ui-react';
 import { checkAuthentication } from '../helpers';
 
 export default withAuth(class Navbar extends Component {
@@ -38,10 +50,10 @@ export default withAuth(class Navbar extends Component {
               &nbsp;
               Okta-React Sample Project
             </Menu.Item>
+            {this.state.authenticated === true && <Menu.Item id="messages-button" as="a" href="/messages"><Icon name="mail outline" />Messages</Menu.Item>}
             {this.state.authenticated === true && <Menu.Item id="profile-button" as="a" href="/profile">Profile</Menu.Item>}
             {this.state.authenticated === true && <Menu.Item id="logout-button" as="a" onClick={this.logout}>Logout</Menu.Item>}
             {this.state.authenticated === false && <Menu.Item as="a" onClick={this.login}>Login</Menu.Item>}
-            {this.state.authenticated === true && <Menu.Item id="messages-button" as="a" href="/messages"><Icon name="mail outline" />Messages</Menu.Item>}
           </Container>
         </Menu>
       </div>
--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,3 +1,4 @@
+
 /* eslint-disable */
 // In production, we register a service worker to serve assets from local cache.
 
