fe549f7fbe37df8921bc1a5a091442919906d31b
Alex Barron <abarron4544@yahoo.com>
2019-03-30 14:01:09 -0600

    possible working with samples.confit

---
+++ client/src/.samples.config.js
@@ -0,0 +1,8 @@
+export default {
+  oidc: {
+    clientId: '0oadjrwyhSqaqWDTz356',
+    issuer: 'https://dev-817020.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:3000/implicit/callback',
+    scope: 'openid profile email',
+  },
+};
