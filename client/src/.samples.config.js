000fee9137bb2558eecc8ed2a691626d7329ee9c
Alex Barron <abarron4544@yahoo.com>
2019-03-31 14:21:51 -0600

    fix heroku problem

--- client/src/.samples.config.js
+++ client/src/.samples.config.js
@@ -2,7 +2,7 @@ export default {
   oidc: {
     clientId: '0oadjrwyhSqaqWDTz356',
     issuer: 'https://dev-817020.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:3000/implicit/callback',
+    redirectUri: 'https://e-motions.herokuapp.com/implicit/callback',
     scope: 'openid profile email',
   },
 };
