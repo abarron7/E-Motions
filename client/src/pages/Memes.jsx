b594e419f38ddd2153653bf307da8365f3a8dee7
Alex Barron <abarron4544@yahoo.com>
2019-03-31 12:15:58 -0600

    Wroking audio autoplay on memes page load

--- client/src/pages/Memes.jsx
+++ client/src/pages/Memes.jsx
@@ -1,14 +1,14 @@
 // Contains the React JSX New Memes page
 // Contains the functions and components/elements required for this page
 
-
 // Okta
-import { withAuth } from '@okta/okta-react';
+import { withAuth } from "@okta/okta-react";
 // React
-import React, { Component } from 'react';
+
+import React, { Component } from "react";
 // Semantic UI
-import { Button, Header } from 'semantic-ui-react';
-import { checkAuthentication } from '../helpers';
+import { Button, Header } from "semantic-ui-react";
+import { checkAuthentication } from "../helpers";
 // Import reusable components utilized in this page
 import { List } from "../components/List/index";
 import Meme from "../components/Meme/index";
@@ -16,158 +16,136 @@ import MemeContainer from "../components/MemeContainer/index";
 // Import API methods to trigger proxy routes
 import API from "../utils/API";
 // Import page specific CSS
-import "./Memes.css"
-
+import "./Memes.css";
+// audio player
+import ReactAudioPlayer from "react-audio-player";
+import soundFile from "./wow.mp3";
 // var $ = require('jquery');
+export default withAuth(
+  class Memes extends Component {
+    constructor(props) {
+      super(props);
+      // this.state = { authenticated: null, userinfo: null };
+      this.checkAuthentication = checkAuthentication.bind(this);
+      this.login = this.login.bind(this);
 
-export default withAuth(class Memes extends Component {
-  constructor(props) {
-    super(props);
-    // this.state = { authenticated: null, userinfo: null };
-    this.checkAuthentication = checkAuthentication.bind(this);
-    this.login = this.login.bind(this);
+      // testing routes START
+      this.state = {
+        authenticated: null,
+        userinfo: null,
+        ready: false,
+        scrapedMemes: []
+      };
+      this.handleSubmit = this.handleSubmit.bind(this);
+      // testing routes END
+    }
 
     // testing routes START
-    this.state = {
-      authenticated: null,
-      userinfo: null,
-      ready: false,
-      scrapedMemes: []
-    };
-    this.handleSubmit = this.handleSubmit.bind(this);
+    handleSubmit(event) {
+      event.preventDefault();
+    }
     // testing routes END
-  }
 
-  // testing routes START
-  handleSubmit(event) {
-    event.preventDefault();
-  }
-  // testing routes END
+    async componentDidMount() {
+      await this.checkAuthentication();
+      this.applyClaims();
+      this.scrapeMemes();
+      console.log(this.state.userinfo);
+    }
 
-  async componentDidMount() {
+    async componentDidUpdate() {
+      await this.checkAuthentication();
+      this.applyClaims();
+    }
+    // Use this to access the unique id from the above
+    // {this.state.claims.map((claimEntry) => {
+    //   const claimName = claimEntry[0];
+    //   const claimValue = claimEntry[1];
+    //   const claimId = `claim-${claimName}`;
+    //   console.log(claimEntry)
+    //   return <tr key={claimName}><td>{claimName}</td><td id={claimId}>{claimValue}</td></tr>;
+    // })}
 
-    await this.checkAuthentication();
-    this.applyClaims();
-    this.scrapeMemes();
-    console.log(this.state.userinfo);
-  }
+    async login() {
+      this.props.auth.login("/");
+    }
 
-  async componentDidUpdate() {
-    await this.checkAuthentication();
-    this.applyClaims();
+    async applyClaims() {
+      if (this.state.userinfo && !this.state.claims) {
+        const claims = Object.entries(this.state.userinfo);
+        this.setState({ claims, ready: true });
+      }
+    }
 
-  }
-  // Use this to access the unique id from the above
-  // {this.state.claims.map((claimEntry) => {
-  //   const claimName = claimEntry[0];
-  //   const claimValue = claimEntry[1];
-  //   const claimId = `claim-${claimName}`;
-  //   console.log(claimEntry)
-  //   return <tr key={claimName}><td>{claimName}</td><td id={claimId}>{claimValue}</td></tr>;
-  // })}
+    scrapeMemes = () => {
+      API.scrapeMemes()
+        .then(res => {
+          this.setState({
+            scrapedMemes: res.data
+          });
+          // console.log("res");
+          // console.log(res);
+        })
+        .catch(err => console.log(err));
+    };
 
-  async login() {
-    this.props.auth.login('/');
-  }
+    saveMeme = () => {
+      API.saveMeme()
+        .then(res => {
+          this.setState({
+            // scrapedMemes: res.data,
+          });
+          console.log("res");
+          console.log(res);
+        })
+        .catch(err => console.log(err));
+    };
 
-  async applyClaims() {
-    if (this.state.userinfo && !this.state.claims) {
-      const claims = Object.entries(this.state.userinfo);
-      this.setState({ claims, ready: true });
+    render() {
+      return (
+        <div className="body-memesfeed">
+          {this.state.authenticated !== null && (
+            <div>
+              {/* <Header as="h1">Custom Login Page with Sign In Widget</Header> */}
+              {this.state.authenticated && (
+                <div>
+                  <ReactAudioPlayer src={soundFile} autoPlay />
+                  {/* <button onClick={() => this.scrapeMemes()}>Click Me</button> */}
+                  {/* <p>Length is {this.state.scrapedMemes.length}</p> */}
+                  <p>
+                    The array is currently {this.state.scrapedMemes.length}{" "}
+                    memes long
+                  </p>
+                  {this.state.scrapedMemes.length ? (
+                    <List>
+                      {this.state.scrapedMemes.map(meme => (
+                        <React.Fragment>
+                          <Meme src={meme} />
+                          <div className="button-rate button-rate-up">YES</div>
+                          <div className="button-rate button-rate-down">NO</div>
+                        </React.Fragment>
+                      ))}
+                    </List>
+                  ) : (
+                    <div className="mockup-content">
+                      <h4 className="heading-title text-center">
+                        <div />
+                      </h4>
+                    </div>
+                  )}
+                </div>
+              )}
+              {!this.state.authenticated && (
+                <div>
+                  <Button id="login-button" primary onClick={this.login}>
+                    Login
+                  </Button>
+                </div>
+              )}
+            </div>
+          )}
+        </div>
+      );
     }
   }
-
-  scrapeMemes = () => {
-    API.scrapeMemes()
-    .then(res => {
-      this.setState({
-        scrapedMemes: res.data,
-      });
-      // console.log("res");
-      // console.log(res);
-    })
-    .catch(err => console.log(err));
-  };
-
-  saveMeme = () => {
-    API.saveMeme()
-    .then(res => {
-      this.setState({
-        // scrapedMemes: res.data,
-      });
-      console.log("res");
-      console.log(res);
-    })
-    .catch(err => console.log(err));
-  }
-
-  render() {
-    const resourceServerExamples = [
-      {
-        label: 'Node/Express Resource Server Example',
-        url: 'https://github.com/okta/samples-nodejs-express-4/tree/master/resource-server',
-      },
-      {
-        label: 'Java/Spring MVC Resource Server Example',
-        url: 'https://github.com/okta/samples-java-spring-mvc/tree/master/resource-server',
-      },
-    ];
-
-    return (
-      <div className="body-memesfeed">
-        {this.state.authenticated !== null &&
-          <div>
-            {/* <Header as="h1">Custom Login Page with Sign In Widget</Header> */}
-            {this.state.authenticated &&
-              <div>
-                {/* <button onClick={() => this.scrapeMemes()}>Click Me</button> */}
-                {/* <p>Length is {this.state.scrapedMemes.length}</p> */}
-                <p>The array is currently {this.state.scrapedMemes.length} memes long</p>
-                {this.state.scrapedMemes.length ? (
-                  <List>
-                    {this.state.scrapedMemes.map(meme => (
-                      <React.Fragment>
-                        <Meme
-                          src={meme}
-                        />
-                        <div className="button-rate button-rate-up">YES</div>
-                        <div className="button-rate button-rate-down">NO</div>
-                      </React.Fragment>
-                    ))}
-                  </List>
-                ) : (
-                  <div className="mockup-content">
-                    <h4 className="heading-title text-center">
-                      <div></div>
-                    </h4>
-                  </div>
-                )}
-              </div>
-            }
-            {!this.state.authenticated &&
-              <div>
-                <p>If you&lsquo;re viewing this page then you have successfully started this React application.</p>
-                <p>
-                  <span>This example shows you how to use the </span>
-                  <a href="https://github.com/okta/okta-oidc-js/tree/master/packages/okta-react">Okta React Library</a>
-                  <span> and the </span>
-                  <a href="https://github.com/okta/okta-signin-widget">Okta Sign-In Widget</a>
-                  <span> to add the </span>
-                  <a href="https://developer.okta.com/authentication-guide/implementing-authentication/implicit">Implicit Flow</a>
-                  <span> to your application. This combination is useful when you want to leverage the features of the Sign-In Widget, </span>
-                  <span> and the authentication helper components from the <code>okta-react</code> module.</span>
-                </p>
-                <p>
-                  Once you have logged in you will be redirected through your authorization server (the issuer defined in config) to create a session for Single-Sign-On (SSO).
-                  After this you will be redirected back to the application with an ID token and access token.
-                  The tokens will be stored in local storage for future use.
-                </p>
-                <Button id="login-button" primary onClick={this.login}>Login</Button>
-              </div>
-            }
-        </div>
-        }
-      </div>
-    );
-  }
-});
+);
--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,7 @@ router.use("/api", apiRoutes);
 
 // If no API routes are hit, send the React app
 router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "../../frontend/src/pages/Home.jsx"));
+  res.sendFile(path.join(__dirname, "../client/src/pages/Home.jsx"));
 });
 
 module.exports = router;
