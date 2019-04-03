383b8d85bb214bf5cc4da9abc9e52f1557d17ac0
Dave Ellis <dgellis@live.com>
2019-04-02 23:28:44 -0600

    Added Saved page

---
+++ client/src/components/MiniMemeImg/index.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import "./style.css";
+
+class MiniMemeImg extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <>
+        {/* <div className="miniMemeContainer"> */}
+          <img
+            className="miniMemeImage"
+            src={this.props.src}
+            alt="meme"
+            onClick={() => this.props.handleOpenMeme()}
+          />
+        {/* </div> */}
+      </>
+    );
+  }
+};
+
+export default MiniMemeImg;
