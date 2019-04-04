0a7be0dde595e2c9f386d6d4831ada2c41badd8b
Alex Barron <abarron4544@yahoo.com>
2019-04-03 23:39:21 -0600

    little fix

--- client/src/.samples.config.js
+++ client/src/.samples.config.js
@@ -3,7 +3,7 @@ export default {
     clientId: '0oadjrwyhSqaqWDTz356',
     issuer: 'https://dev-817020.okta.com/oauth2/default',
     redirectUri: 'https://e-motions.herokuapp.com/implicit/callback',
-    // top is for when push to heroku bottom is for local run
+    // top is for when push to heroku bottom is for local run switch to emotions when pushing to master
     // redirectUri: 'http://localhost:3000/implicit/callback',
     scope: 'openid profile email',
   },
