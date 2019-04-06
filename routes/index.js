f9f4f90ef0290aff365fd2a7799541ed5072449c
Alex Barron <abarron4544@yahoo.com>
2019-04-06 00:57:00 -0600

    possible fix pt 3

--- routes/index.js
+++ routes/index.js
@@ -15,8 +15,8 @@ const apiRoutes = require("./api");
 router.use("/api", apiRoutes);
 
 // If no API routes are hit, send the React app
-// router.use(function(req, res) {
-//   res.sendFile(path.join(__dirname, "../client/src/pages/Home.jsx"));
-// });
+router.use(function(req, res) {
+  res.sendFile(path.join(__dirname, "../client/src/pages/Home.jsx"));
+});
 
 module.exports = router;
